db15aa5bc94916be3180c1b22c7f52dc1b1559f7
GabrielXD <gttome6@gmail.com>
2026-06-15 13:07:41 -0300

    fix: inclui pasta de migrations no repositorio

---
+++ Template/Migrations/20260608230849_InicialEmprestimos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Template.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialEmprestimos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emprestimos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdLivro = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdLeitor = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataEmprestimo = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataDevolucao = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emprestimos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emprestimos");
+        }
+    }
+}
